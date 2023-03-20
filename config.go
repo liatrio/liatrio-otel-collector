d96acffe067cd3eaac7193c4a66111c58f7c6b0f
Adriel Perkins <adriel.perkins@wnco.com>
2023-03-19 23:16:07 -0400

    chore: cleanup and comment through the config.go file

--- config.go
+++ config.go
@@ -5,21 +5,20 @@ import (
 	"time"
 )
 
-//import (
-//	"go.opentelemetry.io/collector/receiver/scraperhelper"
-//)
-
+// Configuration that is exposed to this ldapreceiver through the OTEL config.yaml
 type Config struct {
-	Interval       string `mapstructure:"interval"`
-	ASMEnabled     bool   `mapstructure:"asm_enabled"`
-	UserSecretName string `mapstructure:"user_secret_name"`
-	PassSecretName string `mapstructure:"pass_secret_name"`
+	Interval string `mapstructure:"interval"`
+
+	// TODO: either implement ASM authenticator, or leverage the basic auth extension (preferrable)
+	//ASMEnabled bool   `mapstructure:"asm_enabled"`
+	//UserSecretName string `mapstructure:"user_secret_name"`
+	//PassSecretName string `mapstructure:"pass_secret_name"`
 }
 
+// Validate the configuration passed through the OTEL config.yaml
 func (cfg *Config) Validate() error {
 	interval, _ := time.ParseDuration(cfg.Interval)
 	if interval.Seconds() < 10 {
-		//if cfg.Interval < 10 {
 		return fmt.Errorf("interval must be at least 10 seconds")
 	}
 	return nil
