90ae6b151873ef1b3d8f20c816bcd00fea7e6765
Devin W. Leaman <git@4lch4.email>
2023-09-05 19:34:56 -0500

    refactor: branch count is retrieved w/ go routines
    This commit refactors the section for gathering branch count to use Go routines instead. This cut the run time from 10m to 1m15s in my tests.

--- pkg/receiver/gitproviderreceiver/internal/scraper/gitlabscraper/gitlab_scraper.go
+++ pkg/receiver/gitproviderreceiver/internal/scraper/gitlabscraper/gitlab_scraper.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/Khan/genqlient/graphql"
@@ -56,7 +57,33 @@ func newGitLabScraper(
 	}
 }
 
-// scrape and return metrics
+type branchData struct {
+	ProjectPath string
+	BranchNames []string
+}
+
+// Returns a struct with the project path and an array of branch names via the given channel.
+//
+// The max number of branch names currently returned is 100,000 since the GitLab API will let us get
+// as much as we want but it will take a long time to get all the branches. The biggest repo we
+// found was a GitLab repo with just over 100,000 branches which is an extreme edge case which we
+// believe is not worth supporting.
+func getBranches(
+	ctx context.Context,
+	graphClient graphql.Client,
+	projectPath string,
+	ch chan branchData,
+	waitGroup *sync.WaitGroup,
+) error {
+	defer waitGroup.Done()
+	branches, err := getBranchNames(context.Background(), graphClient, projectPath)
+
+	ch <- branchData{ProjectPath: projectPath, BranchNames: branches.Project.Repository.BranchNames}
+
+	return err
+}
+
+// Scrape the GitLab GraphQL API for the various metrics. took 9m56s to complete.
 func (gls *gitlabScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
 	gls.logger.Sugar().Debug("checking if client is initialized")
 	if gls.client == nil {
@@ -108,26 +135,33 @@ func (gls *gitlabScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
 		}
 	}
 
-	// A map that maps a project name to an array of branch names.
-	var branchNames map[string][]string = make(map[string][]string, len(projectList))
+	ch := make(chan branchData)
+	var wg sync.WaitGroup
 
-	// TODO: Must account for when there are more than 100,000 branch names in a project.
 	for _, project := range projectList {
-		branches, err := getBranchNames(context.Background(), graphClient, project.Path)
-		if err != nil {
-			gls.logger.Sugar().Errorf("error: %v", err)
-		}
+		wg.Add(1)
 
-		branchCount := int64(len(branches.Project.Repository.BranchNames))
-
-		branchNames[project.Name] = branches.Project.Repository.BranchNames
-		gls.mb.RecordGitRepositoryBranchCountDataPoint(now, branchCount, project.Name)
-		gls.logger.Sugar().Debug("branch count: ", branchCount)
+		// TODO: Must account for when there are more than 100,000 branch names in a project.
+		go getBranches(ctx, graphClient, project.Path, ch, &wg)
 	}
 
-	// record metrics
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for branch := range ch {
+		branchCount := int64(len(branch.BranchNames))
+
+		gls.mb.RecordGitRepositoryBranchCountDataPoint(now, branchCount, branch.ProjectPath)
+		gls.logger.Sugar().Debugf("%s branch count: %v", branch.ProjectPath, branchCount)
+	}
+
+	// record repository count metric
 	gls.mb.RecordGitRepositoryCountDataPoint(now, int64(len(projectList)))
 	gls.logger.Sugar().Debugf("metrics: %v", gls.cfg.Metrics.GitRepositoryCount)
+
+	gls.logger.Sugar().Infof("took %v\n", time.Since(now.AsTime()))
 
 	return gls.mb.Emit(), nil
 }
