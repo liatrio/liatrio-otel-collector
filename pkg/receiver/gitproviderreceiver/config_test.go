2a0eb5f61b67ffa6825e132ccd044e0be06a984f
Nicholas Shaddox <Nicholas.Shaddox@liatrio.com>
2023-10-04 11:22:58 -0700

    everyhting is shit

--- pkg/receiver/gitproviderreceiver/config_test.go
+++ pkg/receiver/gitproviderreceiver/config_test.go
@@ -7,16 +7,14 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/confmap"
-	"go.opentelemetry.io/collector/otelcol/otelcoltest"
-	"go.opentelemetry.io/collector/receiver/scraperhelper"
-
 	"github.com/liatrio/liatrio-otel-collector/pkg/receiver/gitproviderreceiver/internal"
 	"github.com/liatrio/liatrio-otel-collector/pkg/receiver/gitproviderreceiver/internal/metadata"
 	"github.com/liatrio/liatrio-otel-collector/pkg/receiver/gitproviderreceiver/internal/scraper/githubscraper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/otelcol/otelcoltest"
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
 )
 
 func TestLoadConfig(t *testing.T) {
@@ -52,27 +50,59 @@ func TestLoadConfig(t *testing.T) {
 	}
 
 	assert.Equal(t, expectedConfig, r1)
+
+	// p := (*confmap.Conf)(nil)
+	// r2 := cfg.Unmarshal(p)
+	// expected := nil
+	// expectedConfig := &Config{
+	// 	ScraperControllerSettings: scraperhelper.ScraperControllerSettings{
+	// 		CollectionInterval: 30 * time.Second,
+	// 		InitialDelay:       1 * time.Second,
+	// 	},
+	// 	Scrapers: map[string]internal.Config{
+	// 		githubscraper.TypeStr: (&githubscraper.Factory{}).CreateDefaultConfig(),
+	// 	},
+	// }
+
+	// assert.Equal(t, expectedConfig, r1)
 }
 
-func TestLoadConfig_CompnentParcerNil(t *testing.T) {
-	factories, err := otelcoltest.NopFactories()
-	require.NoError(t, err)
+// func TestLoadConfig_CompnentParcerNil(t *testing.T) {
+// 	factories, err := otelcoltest.NopFactories()
+// 	require.NoError(t, err)
 
-	factory := NewFactory()
-	factories.Receivers[metadata.Type] = factory
+// 	factory := NewFactory()
+// 	factories.Receivers[metadata.Type] = factory
 
-	// p := confmap.New()
-	p := (*confmap.Conf)(nil)
-	var cfg = (*Config)(nil)
-	err = cfg.Unmarshal(p)
+// 	// Is this mocking?
+// 	p := (*confmap.Conf)(nil)
+// 	cfg := (*Config)(nil)
+// 	err = cfg.Unmarshal(p)
 
-	// cfg, err := otelcoltest.LoadConfigAndValidate(filepath.Join("testdata", "config-nilconfig.yaml"), factories)
+// 	require.NoError(t, err)
 
-	require.NoError(t, err)
-	require.NotNil(t, cfg)
+// 	// assert.Equal(t, len(cfg.Receivers), 2)
+// }
 
-	// assert.Equal(t, len(cfg.Receivers), 2)
-}
+// // Mock componentParser.Unmarshal()
+// func TestLoadConfig_CompnentParcerError(t *testing.T) {
+// 	factories, err := otelcoltest.NopFactories()
+// 	require.NoError(t, err)
+
+// 	factory := NewFactory()
+// 	factories.Receivers[metadata.Type] = factory
+
+// 	// cfg, err := otelcoltest.LoadConfig(filepath.Join("testdata", "config-nilconfig.yaml"), factories)
+// 	componentParser := (*confmap.Conf)
+// 	cfg := (*Config)(nil)
+
+// 	err = cfg.Unmarshal(componentParser)
+
+// 	// require.Error(t, err)
+// 	// require.NotNil(t, cfg)
+
+// 	// assert.Equal(t, len(cfg.Receivers), 2)
+// }
 
 func TestLoadInvalidConfig_NoScrapers(t *testing.T) {
 	factories, err := otelcoltest.NopFactories()
@@ -84,3 +114,49 @@ func TestLoadInvalidConfig_NoScrapers(t *testing.T) {
 
 	require.Contains(t, err.Error(), "must specify at least one scraper")
 }
+
+// func TestConfig_Unmarshal(t *testing.T) {
+// 	factories, err := otelcoltest.NopFactories()
+// 	require.NoError(t, err)
+
+// 	factory := NewFactory()
+// 	factories.Receivers[metadata.Type] = factory
+// 	cfg, err := otelcoltest.LoadConfigAndValidate(filepath.Join("testdata", "config.yaml"), factories)
+
+// 	type fields struct {
+// 		ScraperControllerSettings scraperhelper.ScraperControllerSettings
+// 		Scrapers                  map[string]internal.Config
+// 		MetricsBuilderConfig      metadata.MetricsBuilderConfig
+// 	}
+
+// 	type args struct {
+// 		componentParser *confmap.Conf
+// 	}
+
+// 	tests := []struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name:    "Empty Component Parser",
+// 			fields:  fields{},
+// 			args:    args{componentParser: nil},
+// 			wantErr: false,
+// 		},
+// 		// {
+// 		// 	name: "valid Compenent Parser",
+// 		// 	fields: fields{},
+// 		// 	args: args{ componentParser: notnil },
+// 		// 	wantErr: false,
+// 		// },
+// 	}
+// 	for _, test := range tests {
+// 		t.Run(test.name, func(t *testing.T) {
+// 			if err := cfg.Unmarshal(test.args.componentParser); (err != nil) != test.wantErr {
+// 				t.Errorf("Config.Unmarshal() error = %v, wantErr %v", err, test.wantErr)
+// 			}
+// 		})
+// 	}
+// }
