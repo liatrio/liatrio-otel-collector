e89d5678d692177a14ac2ea79458a9567998941f
jknight-liatrio <43484732+jknight-liatrio@users.noreply.github.com>
2023-10-03 18:26:19 -0700

    save: kinda working mocks for graphql.Client?
    next to test if we can mock our function directly

--- pkg/receiver/gitproviderreceiver/internal/scraper/githubscraper/github_scraper_test.go
+++ pkg/receiver/gitproviderreceiver/internal/scraper/githubscraper/github_scraper_test.go
@@ -5,7 +5,13 @@ package githubscraper
 
 import (
 	"context"
+	"github.com/Khan/genqlient/graphql"
+	"github.com/liatrio/liatrio-otel-collector/pkg/receiver/gitproviderreceiver/internal/scraper/githubscraper/mocks"
+	"github.com/stretchr/testify/mock"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/receiver"
@@ -18,4 +24,44 @@ func TestNewGitHubScraper(t *testing.T) {
 	s := newGitHubScraper(context.Background(), receiver.CreateSettings{}, defaultConfig.(*Config))
 
 	assert.NotNil(t, s)
+}
+
+func TestGetPullRequests(t *testing.T) {
+	factory := Factory{}
+	defaultConfig := factory.CreateDefaultConfig()
+
+	s := newGitHubScraper(context.Background(), receiver.CreateSettings{}, defaultConfig.(*Config))
+
+	client := mocks.NewClient(t)
+	client.On("MakeRequest", mock.AnythingOfType("context.backgroundCtx"), mock.AnythingOfType("*graphql.Request"), mock.AnythingOfType("*graphql.Response")).
+		Return(func(ctx context.Context, req *graphql.Request, resp *graphql.Response) error {
+			var data getPullRequestCountResponse
+			var err error
+
+			resp = &graphql.Response{Data: &data}
+
+			return err
+		})
+
+	ctx := context.Background()
+	repos := make([]SearchNode, 5)
+	now := pcommon.NewTimestampFromTime(time.Now())
+
+	var wg sync.WaitGroup
+	pullRequests := make(chan []PullRequestNode)
+
+	wg.Add(2)
+	s.getPullRequests(
+		ctx,
+		client,
+		repos,
+		now,
+		pullRequests,
+		&wg,
+	)
+	wg.Wait()
+	close(pullRequests)
+
+	assert.NotNil(t, s)
+
 }
