7e15f15140d0160f49633a8ee005593d6730c923
densellp <densell321@gmail.com>
2023-12-01 11:30:44 -0800

    remove: MyAwesomeReceiver2 generated code

--- pkg/receiver/MyAwesomeReceiver2/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate mdatagen metadata.yaml
-
-package MyAwesomeReceiver // import "github.com/liatrio/liatrio-otel-collector/pkg/receiver/MyAwesomeReceiver"
