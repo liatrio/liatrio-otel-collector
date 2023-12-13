14c2182383dc2336085ca304d5cb2c28ede12ed0
Alex Ramsay <alex.ramsay@liatrio.com>
2023-12-13 15:50:56 -0500

    Add tests for common.Render() in compgen

--- cmd/compgen/cmd/common/common_test.go
+++ cmd/compgen/cmd/common/common_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
@@ -20,3 +21,44 @@ func TestTidy(t *testing.T) {
 	assert.FileExists(t, filepath.Join(dir, "go.mod"))
 }
 
+func TestRender(t *testing.T) {
+	source := t.TempDir()
+	destination := t.TempDir()
+	data := TemplateData{Name: "dummy", PackageName: "github.com/dummy"}
+
+	// Source does not contain a "templates" directory
+	err := Render(os.DirFS(source), destination, data)
+	assert.Error(t, err)
+
+	// Nothing to do
+	os.Mkdir(filepath.Join(source, "templates"), os.ModePerm)
+	err = Render(os.DirFS(source), destination, data)
+	assert.NoError(t, err)
+
+	// Ignore  non-template files
+	os.Create(filepath.Join(source, "templates", "main.go"))
+	err = Render(os.DirFS(source), destination, data)
+	assert.NoError(t, err)
+	assert.NoFileExists(t, filepath.Join(destination, "main.go"))
+
+	// Empty tmpl file
+	file, _ := os.Create(filepath.Join(source, "templates", "main.go.tmpl"))
+	err = Render(os.DirFS(source), destination, data)
+	assert.NoError(t, err)
+	assert.FileExists(t, filepath.Join(destination, "main.go"))
+
+	// Normal tmpl file
+	file.WriteString("pkg {{ .PackageName }}")
+	err = Render(os.DirFS(source), destination, data)
+	assert.NoError(t, err)
+	assert.FileExists(t, filepath.Join(destination, "main.go"))
+	content, _ := os.ReadFile(filepath.Join(destination, "main.go"))
+	assert.Equal(t, "pkg github.com/dummy", string(content))
+
+	// Empty tmpl file
+	os.Mkdir(filepath.Join(source, "templates", "more"), os.ModePerm)
+	os.Create(filepath.Join(source, "templates", "more", "test.go.tmpl"))
+	err = Render(os.DirFS(source), destination, data)
+	assert.NoError(t, err)
+	assert.FileExists(t, filepath.Join(destination, "more", "test.go"))
+}
