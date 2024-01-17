1d7f9049a6076251be9b5c835c6468d6a63d7a68
Adriel Perkins <adrielp@liatrio.com>
2024-01-17 15:56:36 -0500

    chore: fmt tools.go

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -6,18 +6,16 @@ package tools // import "github.com/liatrio/liatrio-otel-collector/internal/tool
 // This file exists to ensure consistent versioning and tooling installs based on
 // https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
-
 import (
-    _ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-    _ "github.com/Khan/genqlient"
-    _ "honnef.co/go/tools/cmd/staticcheck"
-    _ "github.com/securego/gosec/v2/cmd/gosec"
-    _ "golang.org/x/tools/cmd/goimports"
-    _ "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen"
-    _ "golang.org/x/vuln/cmd/govulncheck"
-    _ "go.opentelemetry.io/collector/cmd/builder"
-    _ "github.com/goreleaser/goreleaser"
-    _ "go.opentelemetry.io/build-tools/multimod"
-    _ "go.opentelemetry.io/build-tools/crosslink"
+	_ "github.com/Khan/genqlient"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen"
+	_ "github.com/securego/gosec/v2/cmd/gosec"
+	_ "go.opentelemetry.io/build-tools/crosslink"
+	_ "go.opentelemetry.io/build-tools/multimod"
+	_ "go.opentelemetry.io/collector/cmd/builder"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/vuln/cmd/govulncheck"
+	_ "honnef.co/go/tools/cmd/staticcheck"
 )
-
