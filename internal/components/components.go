d04df3abf0643f52820c2906b4a6ede67d542d14
Adriel Perkins <adrielp@liatrio.com>
2024-01-18 21:03:45 -0500

    chore: run go fmt against components to improve goreport card

--- internal/components/components.go
+++ internal/components/components.go
@@ -1,6 +1,5 @@
 package components
 
 import (
-    _ "github.com/liatrio/liatrio-otel-collector/receiver/gitproviderreceiver"
+	_ "github.com/liatrio/liatrio-otel-collector/receiver/gitproviderreceiver"
 )
-
