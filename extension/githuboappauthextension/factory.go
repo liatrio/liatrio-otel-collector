1004c7d6305f0c634db9f98e10a8bbdae0cfefcb
Adriel Perkins <adriel@adrielperkins.com>
2024-04-23 09:54:32 -0400

    feat: add initial commit of githubappauth extension

---
+++ extension/githuboappauthextension/factory.go
@@ -0,0 +1,37 @@
+package githuboappauthextension // import "github.com/liatrio/liatrio-otel-collector/extension/githubappauthextension"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/extension/auth"
+
+	"github.com/liatrio/liatrio-otel-collector/extension/githubappauthextension/internal/metadata"
+)
+
+// NewFactory creates a factory for the GitHub App Auth extension.
+func NewFactory() extension.Factory {
+	return extension.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		createExtension,
+		metadata.ExtensionStability,
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{}
+}
+
+func createExtension(_ context.Context, set extension.CreateSettings, cfg component.Config) (extension.Extension, error) {
+	ca, err := newClientAuthenticator(cfg.(*Config), set.Logger)
+	if err != nil {
+		return nil, err
+	}
+
+	return auth.NewClient(
+		auth.WithClientRoundTripper(ca.roundTripper),
+		auth.WithClientPerRPCCredentials(ca.perRPCCredentials),
+	), nil
+} 
