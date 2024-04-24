8d555b3f74cff8ecfed1f42b607998ad0ca07e7a
Adriel Perkins <adrielp@liatrio.com>
2024-04-23 22:56:14 -0400

    chore: add config test and update config for github

--- extension/githuboappauthextension/config.go
+++ extension/githuboappauthextension/config.go
@@ -2,71 +2,36 @@ package githuboappauthextension // import "github.com/liatrio/liatrio-otel-colle
 
 import (
 	"errors"
-	"net/url"
-	"time"
 
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/config/configopaque"
-	"go.opentelemetry.io/collector/config/configtls"
 )
 
 var (
 	errNoGitHubAppIDProvided      = errors.New("no GitHub App ID provided in the GitHub App Auth extension configuration")
 	errNoGitHubPrivateKeyProvided = errors.New("no GitHub App Private Key provided in the GitHub App Auth extension configuration")
-	errNoClientIDProvided         = errors.New("no ClientID provided in the GitHub App Auth exporter configuration")
-	errNoTokenURLProvided         = errors.New("no TokenURL provided in OAuth Client Credentials configuration")
-	errNoClientSecretProvided     = errors.New("no ClientSecret provided in OAuth Client Credentials configuration")
 )
 
-// Config stores the configuration for GitHub App Auth Client Credentials (2-legged GitHub App Auth flow) setup.
+// Config store the configuration for the GitHub App Installation flow. See:
+// https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/authenticating-as-a-github-app-installation
 type Config struct {
 
-	// ClientID is the application's ID.
-	// See https://datatracker.ietf.org/doc/html/rfc6749#section-2.2
-	ClientID string `mapstructure:"client_id"`
+    // GitHubAppID is the ID of the GitHub App.
+	GitHubAppID string `mapstructure:"client_id"`
 
-	// ClientIDFile is the file path to read the application's ID from.
-	ClientIDFile string `mapstructure:"client_id_file"`
-
-	// ClientSecret is the application's secret.
-	// See https://datatracker.ietf.org/doc/html/rfc6749#section-2.3.1
-	ClientSecret configopaque.String `mapstructure:"client_secret"`
-
-	// ClientSecretFile is the file pathg to read the application's secret from.
-	ClientSecretFile string `mapstructure:"client_secret_file"`
-
-	// EndpointParams specifies additional parameters for requests to the token endpoint.
-	EndpointParams url.Values `mapstructure:"endpoint_params"`
-
-	// TokenURL is the resource server's token endpoint
-	// URL. This is a constant specific to each server.
-	// See https://datatracker.ietf.org/doc/html/rfc6749#section-3.2
-	TokenURL string `mapstructure:"token_url"`
-
-	// Scope specifies optional requested permissions.
-	// See https://datatracker.ietf.org/doc/html/rfc6749#section-3.3
-	Scopes []string `mapstructure:"scopes,omitempty"`
-
-	// TLSSetting struct exposes TLS client configuration for the underneath client to authorization server.
-	TLSSetting configtls.ClientConfig `mapstructure:"tls,omitempty"`
-
-	// Timeout parameter configures `http.Client.Timeout` for the underneath client to authorization
-	// server while fetching and refreshing tokens.
-	Timeout time.Duration `mapstructure:"timeout,omitempty"`
+    // GitHubAppPrivateKeyFile is the file path to the private key generated
+    // for the GitHub App.
+	GitHubAppPrivateKeyFile string `mapstructure:"client_secret_file"`
 }
 
 var _ component.Config = (*Config)(nil)
 
 // Validate checks if the extension configuration is valid
 func (cfg *Config) Validate() error {
-	if cfg.ClientID == "" && cfg.ClientIDFile == "" {
-		return errNoClientIDProvided
+	if cfg.GitHubAppID == "" {
+		return errNoGitHubAppIDProvided
 	}
-	if cfg.ClientSecret == "" && cfg.ClientSecretFile == "" {
-		return errNoClientSecretProvided
-	}
-	if cfg.TokenURL == "" {
-		return errNoTokenURLProvided
+	if cfg.GitHubAppPrivateKeyFile == "" {
+		return errNoGitHubPrivateKeyProvided
 	}
 	return nil
 }
