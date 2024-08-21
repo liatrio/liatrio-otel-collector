254d694e278f79adee5298bdbfd4dbff46cf9a67
Adriel Perkins <adrielp@liatrio.com>
2024-08-21 15:44:37 -0400

    feat(yolo): removing components to allow for properly versioned comps?

--- internal/components/components.go
+++
@@ -1,6 +0,0 @@
-package components
-
-import (
-	_ "github.com/liatrio/liatrio-otel-collector/receiver/gitproviderreceiver"
-	_ "github.com/liatrio/liatrio-otel-collector/extension/githubappauthextension"
-)
