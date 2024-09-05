0959254df769ceaa804d3d8c07b0c315ec363a64
gnmeyer <grant.meyer@liatrio.com>
2024-09-05 10:18:08 -0700

    feat: separate github receiver into separate component

---
+++ receiver/githubreceiver/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+package githubreceiver // import "github.com/liatrio/liatrio-otel-collector/receiver/githubreceiver"
