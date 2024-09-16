894a65ab73cecdaf77bad2c86dd423bdea6028fd
gnmeyer <77987842+gnmeyer@users.noreply.github.com>
2024-09-16 13:40:05 -0700

    feat(gitlab): migrate gitproviderreceiver gitlab scraper to gitlabreceiver (#524)
    * feat: separate gitlab receiver into separate component
    
    * fix: update go:generate path to mdatagen binary
    
    Updated the go:generate directive in doc.go to include the correct relative path to the mdatagen binary located in the .tools directory at the root of the project. This change fixes a make generate error caused by the previous incorrect path.
    
    * fix: make test-all and add receiver to component
    
    * fix: run go tidy-all
    
    * Update receiver/gitlabreceiver/internal/scraper/gitlabscraper/factory.go
    
    Signed-off-by: Adriel Perkins <adrielp@liatrio.com>
    
    * chore: run go fmt on factory.go
    
    ---------
    
    Signed-off-by: Adriel Perkins <adriel@adrielperkins.com>
    Signed-off-by: Adriel Perkins <adrielp@liatrio.com>
    Co-authored-by: Adriel Perkins <adriel@adrielperkins.com>

---
+++ receiver/gitlabreceiver/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate ../../.tools/mdatagen metadata.yaml
+
+package gitlabreceiver // import "github.com/liatrio/liatrio-otel-collector/receiver/gitlabreceiver"
---
+++ receiver/gitlabreceiver/internal/scraper/gitlabscraper/gitlab_scraper_test.go
@@ -0,0 +1,161 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package gitlabscraper
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/liatrio/liatrio-otel-collector/receiver/gitlabreceiver/internal/metadata"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/pmetrictest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xanzy/go-gitlab"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/receiver/receivertest"
+)
+
+/*
+ * Testing for newGitLabScraper
+ */
+func TestNewGitLabScraper(t *testing.T) {
+	factory := Factory{}
+	defaultConfig := factory.CreateDefaultConfig()
+
+	s := newGitLabScraper(context.Background(), receiver.Settings{}, defaultConfig.(*Config))
+
+	assert.NotNil(t, s)
+}
+
+func TestScrape(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		expectedErr   error
+		server        *http.ServeMux
+		expectedCount int
+		testFile      string
+	}{
+		{
+			desc: "No Projects",
+			server: MockServer(&responses{
+				projectResponse: projectResponse{
+					projects:     []*gitlab.Project{},
+					responseCode: http.StatusOK,
+				},
+			}),
+			testFile:    "expected_no_projects.yaml",
+			expectedErr: errors.New("no GitLab projects found for the given group/org: project"),
+		},
+		{
+			desc: "Happy Path",
+			server: MockServer(&responses{
+				projectResponse: projectResponse{
+					projects: []*gitlab.Project{
+						{
+							Name:              "project",
+							PathWithNamespace: "project",
+							CreatedAt:         gitlab.Ptr(time.Now().AddDate(0, 0, -1)),
+							LastActivityAt:    gitlab.Ptr(time.Now().AddDate(0, 0, -1)),
+						},
+					},
+					responseCode: http.StatusOK,
+				},
+				branchResponse: branchResponse{
+					branches: getBranchNamesProjectRepository{
+						BranchNames: []string{"branch1"},
+					},
+					responseCode: http.StatusOK,
+				},
+				mrResponse: mrResponse{
+					mrs: []getMergeRequestsProjectMergeRequestsMergeRequestConnection{
+						{
+							Nodes: []MergeRequestNode{
+								{
+									Title:     "mr1",
+									CreatedAt: time.Now().AddDate(0, 0, -1),
+								},
+								{
+									Title:    "mr1",
+									MergedAt: time.Now().AddDate(0, 0, -1),
+								},
+							},
+						},
+					},
+					responseCode: http.StatusOK,
+				},
+				contribResponse: contribResponse{
+					contribs: []*gitlab.Contributor{
+						{
+							Name:      "contrib1",
+							Additions: 1,
+							Deletions: 1,
+						},
+					},
+					responseCode: http.StatusOK,
+				},
+				compareResponse: compareResponse{
+					compare: &gitlab.Compare{
+						Commits: []*gitlab.Commit{
+							{
+								Title:     "commit1",
+								CreatedAt: gitlab.Ptr(time.Now().AddDate(0, 0, -1)),
+							},
+						},
+					},
+					responseCode: http.StatusOK,
+				},
+			}),
+			testFile: "expected_happy_path.yaml",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			server := httptest.NewServer(tc.server)
+			defer server.Close()
+
+			cfg := &Config{MetricsBuilderConfig: metadata.DefaultMetricsBuilderConfig()}
+
+			gls := newGitLabScraper(context.Background(), receivertest.NewNopSettings(), cfg)
+			gls.cfg.GitLabOrg = "project"
+			gls.cfg.ClientConfig.Endpoint = server.URL
+
+			err := gls.start(context.Background(), componenttest.NewNopHost())
+			require.NoError(t, err)
+
+			actualMetrics, err := gls.scrape(context.Background())
+			if tc.expectedErr != nil {
+				require.Equal(t, tc.expectedErr, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			expectedFile := filepath.Join("testdata", "scraper", tc.testFile)
+
+			// Due to the generative nature of the code we're using through genqlient. The tests happy path changes,
+			// and needs to be rebuilt to satisfy the unit tests. When the metadata.yaml changes, and code is
+			// introduced, or removed. We'll need to update the metrics by uncommenting the below and running
+			// `make test` to generate it. Then we're safe to comment this out again and see happy tests.
+			// golden.WriteMetrics(t, expectedFile, actualMetrics) // This line is temporary! TODO remove this!!
+
+			expectedMetrics, err := golden.ReadMetrics(expectedFile)
+			require.NoError(t, err)
+
+			require.NoError(t, pmetrictest.CompareMetrics(
+				expectedMetrics,
+				actualMetrics,
+				pmetrictest.IgnoreMetricDataPointsOrder(),
+				pmetrictest.IgnoreTimestamp(),
+				pmetrictest.IgnoreStartTimestamp(),
+			))
+		})
+	}
+}
