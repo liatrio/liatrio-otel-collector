92993a553ddcdbc81644c5c83a1e1f6122366d13
gnmeyer <77987842+gnmeyer@users.noreply.github.com>
2024-09-16 13:02:30 -0700

    feat(github): migrate the gitproviderreceiver githubscraper to githubreceiver (#523)

---
+++ receiver/githubreceiver/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate ../../.tools/mdatagen metadata.yaml
+
+package githubreceiver // import "github.com/liatrio/liatrio-otel-collector/receiver/githubreceiver"
