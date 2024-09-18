fe2a9268c9d78f6cafb530b586dc2f25247df804
Adriel Perkins <adriel@adrielperkins.com>
2024-09-18 10:49:49 -0400

    chore: fix up location of mdatagen

--- receiver/githubactionsreceiver/doc.go
+++ receiver/githubactionsreceiver/doc.go
@@ -1,6 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate mdatagen metadata.yaml
+//go:generate ../../.tools/mdatagen metadata.yaml
 
 package githubactionsreceiver // import "github.com/liatrio/liatrio-otel-collector/receiver/githubactionsreceiver"
