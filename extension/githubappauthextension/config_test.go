00ddd2c2766b3c43defa9c85674f7474ae9f8372
Adriel Perkins <adriel@adrielperkins.com>
2025-02-21 21:21:04 -0500

    chore: tidy and update deps

--- extension/githubappauthextension/config_test.go
+++ extension/githubappauthextension/config_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
+	"go.opentelemetry.io/collector/confmap/xconfmap"
 
 	"github.com/liatrio/liatrio-otel-collector/extension/githubappauthextension/internal/metadata"
 )
@@ -43,10 +44,10 @@ func TestLoadConfig(t *testing.T) {
 			require.NoError(t, err)
 			require.NoError(t, sub.Unmarshal(cfg))
 			if tt.expectedErr != nil {
-				assert.ErrorIs(t, component.ValidateConfig(cfg), tt.expectedErr)
+				assert.ErrorIs(t, xconfmap.Validate(cfg), tt.expectedErr)
 				return
 			}
-			assert.NoError(t, component.ValidateConfig(cfg))
+			assert.NoError(t, xconfmap.Validate(cfg))
 			assert.Equal(t, tt.expected, cfg)
 		})
 	}
